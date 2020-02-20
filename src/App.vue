0e5bef0d94738490544253db29f182f91380201e
user <mancusofrancesca@hotmail.com>
2020-02-20 08:03:46 +0000

    search done

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template lang="html">
   <div>
     <h1>Countries List</h1>
-    <country-detail v-bind:country="selectedCountry"></country-detail>
-    <countries-list v-bind:countries="countries"></countries-list>    
+    <country-search v-bind:countries="countries"></country-search>
+    <countries-list v-bind:countries="countries"></countries-list>
+    <country-detail v-if="selectedCountry" v-bind:country="selectedCountry"></country-detail>
+    <country-detail v-if="searchedCountry" v-bind:country="searchedCountry"></country-detail>    
   </div>
 </template>
 
@@ -10,25 +12,28 @@
 import CountriesList from './components/CountriesList.vue';
 import {eventBus} from './main.js';
 import CountryDetail from './components/CountryDetail.vue';
+import CountrySearch from './components/CountrySearch.vue';
 
 export default {
   name: 'app',
   data(){
     return {
       countries: [],
-      selectedCountry: null
+      selectedCountry: null,
+      searchedCountry: null
     }
   },
   mounted(){
     fetch('https://restcountries.eu/rest/v2/all')
     .then(result => result.json())
     .then(countries => this.countries = countries);
-
-    eventBus.$on('country-selected', (country) => this.selectedCountry = country)
+    eventBus.$on('country-selected', (country) => this.selectedCountry = country);
+    eventBus.$on('country-searched', (country) => this.searchedCountry = country)
   },
   components: {
     "countries-list": CountriesList,
-    "country-detail": CountryDetail
+    "country-detail": CountryDetail,
+    "country-search": CountrySearch
   }
 }
 </script>
---
+++ src/components/CountrySearch.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <input v-on:input="findCountry(countries)" v-model="search.text" type="text">
+</template>
+
+<script>
+
+import {eventBus} from '../main.js';
+
+export default {
+
+  name: 'country-search',
+
+  data(){
+    return {
+      "search": {
+        result: {},
+        text: "" }
+    }
+  },
+
+  props: ['countries'],
+
+  methods: {
+    findCountry(countries){
+      this.search.result = countries.find(country => country.name.includes(this.search.text));
+      eventBus.$emit('country-searched', this.search.result)
+    }
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
